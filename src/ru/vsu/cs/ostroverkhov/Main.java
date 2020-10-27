0276749b59f43e157c5fbd2df9f91acb3c712894
ostand <71151132+ostand@users.noreply.github.com>
2020-10-27 19:03:30 +0300

    Update Main.java

--- src/ru/vsu/cs/ostroverkhov/Main.java
+++ src/ru/vsu/cs/ostroverkhov/Main.java
@@ -4,36 +4,31 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
-      double n = readNumbers('N');
-      double k = readNumbers('K');
+    public static void main(String[] args) {
+        double n = readNumbers('N');
+        double k = readNumbers('K');
 
-      double sum = sumOfNumber(n, k);
+        double sum = sumOfNumber(n, k);
 
-      showResult(sum);
-
+        showResult(sum);
     }
 
-    public static double sumOfNumber (double n, double k)
-    {
-     double sum = 0;
-     for (int i = 1; i <= n; i++)
-     {
-         sum+= Math.pow(i, k);
-     }
-     return sum;
+    public static double sumOfNumber(double n, double k) {
+        double sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += Math.pow(i, k);
+        }
+        return sum;
     }
 
-    public static double readNumbers(char name)
-    {
+    public static double readNumbers(char name) {
         Scanner scanner = new Scanner(System.in);
         System.out.printf("Enter the number %s: ", name);
         return scanner.nextDouble();
     }
 
-    public static void showResult(double sum)
-    {
+    public static void showResult(double sum) {
         System.out.println("Sum = " + sum);
     }
 }
+
